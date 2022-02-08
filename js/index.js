410632ddc92a552e4e62f206142bfa2bf7d87d66
Ciller-Hacker <safarimbetovdilshodbek77@gmail.com>
2022-02-08 19:44:11 +0500

    addto

--- js/index.js
+++ js/index.js
@@ -4,45 +4,51 @@ const elCity = document.querySelector('.js-cutyes')
 const elInput = document.querySelector('.js-input')
 
 
-function handleSubmitBtn(hods){
-        fetch(
-            `http://api.openweathermap.org/geo/1.0/direct?q=${hods.target.textContent}&limit=1&appid=655af4bc6e63602e3711d761a5b7ec5a`
-        )
-        .then((res) => res.json())  
-        // console.log(hods.target.dataset.let);
-        localStorage.setItem('lat',hods.target.dataset.let)
-        localStorage.setItem('lon',hods.target.dataset.lon)
-        window.location = '/info.html'
-    }
-    elWrapper.addEventListener('click', handleSubmitBtn);
+function handleSubmitBtn(hods) {
+    fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${hods.target.textContent}&limit=1&appid=655af4bc6e63602e3711d761a5b7ec5a`
+    )
+        .then((res) => res.json())
+    // console.log(hods.target.dataset.let);
+    localStorage.setItem('lat', hods.target.dataset.let)
+    localStorage.setItem('lon', hods.target.dataset.lon)
+    window.location = '/info.html'
+}
+elWrapper.addEventListener('click', handleSubmitBtn);
 
 
 
-    function inputs(evt){
+function inputs(evt) {
+    evt.preventDefault();
+    elCity.innerHTML = null
+    fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${elInput.value}&limit=5&appid=655af4bc6e63602e3711d761a5b7ec5a`
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach(element => {
+                let elButtons = document.createElement('button')
+                elButtons.textContent = element.name
+                elButtons.classList.add('list-group-item-action')
+                elButtons.classList.add('border-0')
+                elButtons.classList.add('w-100')
+                elButtons.classList.add('px-3')
+                elButtons.classList.add('py-2')
+                
+                elCity.append(elButtons)
 
-        elCity.addEventListener('click', (mol) => {
-            mol.preventDefault();
-
-            fetch(
-                `http://api.openweathermap.org/geo/1.0/direct?q=${elInput.value}&limit=1&appid=655af4bc6e63602e3711d761a5b7ec5a`
-            )
-            .then((res) => res.json())  
-            .then((data) => {
-             data.forEach(element => {
-                 
-
-            localStorage.setItem('lat',element.lat);
-            localStorage.setItem('lon',element.lon);
-                console.log(element.lat ); 
-                console.log(element.lon);
-                window.location = '/info.html'
+                // py-2, px-3, border-0, w-100, 
+                elButtons.addEventListener('click', () => {
+                    localStorage.setItem('lat', element.lat);
+                    localStorage.setItem('lon', element.lon);
+                    console.log(element.lat);
+                    console.log(element.lon);
+                    window.location = '/info.html'
+                })
             })
-           } )
-            // console.log(res.json());
         })
+}
 
-    }
-
-    elInput.addEventListener('click', inputs);
+elInput.addEventListener('input', inputs);
 
 
