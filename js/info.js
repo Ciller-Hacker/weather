6ae7d5626b99d89a257ce590d4060ff8fecff448
Ciller-Hacker <safarimbetovdilshodbek77@gmail.com>
2022-02-09 20:36:57 +0500

    dale

--- js/info.js
+++ js/info.js
@@ -5,6 +5,7 @@
   const elHavo = document.querySelector('.havo')
   const elBulut = document.querySelector('.bulut')
   const elKun = document.querySelector('.container')
+  const ellar = document.querySelector('.kun')
 
 
 let elLet = localStorage.getItem('lat')
@@ -26,33 +27,18 @@ let elLon = localStorage.getItem('lon')
             });
             elBulut.textContent = data.clouds.all
             if(data.clouds.all > 40 && data.clouds.all <80 ){
+              ellar.textContent = 'Bugun kun biroz bulutli boladi'
               elKun.classList.add('bulutli')
             }else if(data.clouds.all >= 80){
+              ellar.textContent = 'Bugun yomgir yogish ehtimoli mavjud'
               elKun.classList.add('yomgir')
             }else if (data.clouds.all<=40) {
+              ellar.textContent = 'Bugun havo ochiq boladi, kun esa quyoshli boladi'
               elKun.classList.add('quyosh')
             }
 
-        })
+        });
 
 
 
 
-
-
-        // fetch(
-        //   `http://api.openweathermap.org/data/2.5/weather?lat=${elLet}&lon=${elLon}&appid=655af4bc6e63602e3711d761a5b7ec5a`
-        //   )
-        //   .then((res) => res.json())
-        //   .then((data) => {
-            
-        //     console.log(data);
-        //     body.textContent = data.name + ' ' + 'City'
-        //     elHar.textContent = Math.ceil(data.main.temp - 273.15) + ' ' + 'C' 
-        //     elSham.textContent = data.wind.speed
-        //     elBosim.textContent = data.main.pressure
-        //     data.weather.forEach(element => {
-        //       elHavo.textContent = element.main
-        //       });
-        //       elBulut.textContent = data.clouds.all
-        //   })
